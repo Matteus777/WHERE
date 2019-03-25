c47bb4a66e87455f17c1afe43ed5858669206a7f
matteus camargo <matteus.camargo@gmail.com>
2019-03-25 00:16:59 -0300

    .

--- app/src/main/java/com/example/whereapplication/FirstActivity.java
+++ app/src/main/java/com/example/whereapplication/FirstActivity.java
@@ -7,29 +7,10 @@ import com.facebook.GraphRequest;
 import com.facebook.AccessToken;
 import com.facebook.HttpMethod;
 import com.facebook.GraphResponse;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationServices
 public class FirstActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_first);
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        AppEventsLogger.activateApp(this);
-
-        private void createLocationRequest(){
-            LocationRequest mLocationRequest = new LocationRequest();
-            mLocationRequest.setInterval(10000);
-            mLocationRequest.setFastestInterval(5000);
-            mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-            LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
-            SettingsClient client = LocationServices.getSettingsClient(this);
-            Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
-        }
-
 
     }
 }
---
+++ app/src/main/java/com/example/whereapplication/LocationProvider.java
@@ -0,0 +1,10 @@
+package com.example.whereapplication;
+
+
+import android.app.Activity;
+import android.content.Context;
+public class LocationProvider {
+
+}
+
+
