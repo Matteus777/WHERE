8a5933cb61df57b9c1249d02081bd3d2ee80d502
fredvjacobi <fredvjacobi@gmail.com>
2019-03-27 14:25:30 -0300

    Start menubar.xml

--- app/src/main/java/com/example/whereapplication/ListActivity.java
+++ app/src/main/java/com/example/whereapplication/ListActivity.java
@@ -2,6 +2,7 @@ package com.example.whereapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class ListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        android.support.v7.widget.Toolbar barra = findViewById(R.id.toolbar);
+        setSupportActionBar(barra);
     }
 }
